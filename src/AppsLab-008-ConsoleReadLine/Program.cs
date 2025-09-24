4184d562cbd3d681942744368f4bb2bc0ae9c6ea
Lukaš Maceašik <fish999mac@gmail.com>
2025-09-24 12:51:09 +0200

    Uprava textu

--- src/AppsLab-008-ConsoleReadLine/Program.cs
+++ src/AppsLab-008-ConsoleReadLine/Program.cs
@@ -1,8 +1,7 @@
 ﻿Console.WriteLine("Ako sa voláš?");
 string? meno = Console.ReadLine();
-Console.WriteLine("Ahoj, " + meno);
+Console.WriteLine("Ahoj, " + meno + " Kolko máš rokov?");
 
-Console.WriteLine("Koľko máš rokov?");
 string vstup = Console.ReadLine() ?? "0";
 int vek = int.Parse(vstup);
-Console.WriteLine("Máš " + vek + " rokov.");
+Console.WriteLine("Aha, máš teda " + vek + " rokov.");
